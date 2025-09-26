ceae963c5abe971d58fda6fcf0297e5a218ca55e
UlyaFed <fedak.ulya99@gmail.com>
2025-09-26 13:49:32 +0200

    CustomUserManager

--- users/models.py
+++ users/models.py
@@ -1,28 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-
-#every user have his own Team 
 class Team(models.Model):
-    
     team_name = models.CharField(max_length=250)
-    manager = models.CharField(max_length=250, blank=True,)
-    
+    manager = models.CharField(max_length=250, blank=True)
     def __str__(self):
         return self.team_name
 
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, null= True, blank=True)
-    username= None
+    username = None
     email = models.EmailField("email address", unique=True)
-    
-    USERNAME_FIELD ="email"
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-    
+
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.email
-    
-    
-    
+
+
