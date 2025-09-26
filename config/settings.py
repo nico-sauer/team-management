f0d429035eede24fe14d5c5953908e7615d64b5c
EdenGoldstein <edengoldstein5@gmail.com>
2025-09-26 09:41:13 +0200

    urls,settings

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    
+    'rest_framework',
+    'phonenumber_field',
+    'plans',
+    'config',
     'users',
+    'profiles',
+    'appointments',
+    'core',
+    
     
 ]
 
@@ -77,22 +86,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.getenv("DB_NAME"),
-    #     'USER': os.getenv("DB_USER"),
-    #     'PASSWORD': os.getenv("DB_PASSWORD"),
-    #     'HOST': os.getenv("DB_HOST"),
-    #     'PORT': os.getenv("DB_PORT")          
-    # }
-    
-      'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")          
     }
     
-    
 }
+
+if "DATABASE_URL" in os.environ:
+    logger.info("Adding $DATABASE_URL to default DATABASE Django setting.")
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+    DATABASES["default"]["init_command"] = "SET sql_mode='STRICT_TRANS_TABLES'"
 
 
 # Password validation
