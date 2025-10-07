ecb0fee0abbd5b4552360e42397631df1e559d74
nico-sauer <nico.sauer@dci-student.org>
2025-10-07 18:52:48 +0200

    profiles wip

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
+from django import forms
 
-from .models import StaffProfile
+from .models import AthleteProfile, StaffProfile
 
 
 class StaffRoles(ModelForm):
@@ -12,3 +13,34 @@ class StaffTitle(ModelForm):
     class Meta:
         model = StaffProfile
         fields = ['title']
+        
+
+
+class BloodType(ModelForm):
+    class Meta:
+        model = AthleteProfile
+        fields = ['blood_type']
+        
+class Gender(ModelForm):
+    class Meta:
+        model = AthleteProfile
+        fields = ['gender']
+        
+        
+class AthleteProfileForm(ModelForm):
+    #allergies = forms.TextInput() 
+    last_updated =forms.DateField(widget=forms.SelectDateWidget)
+    class Meta:
+        model = AthleteProfile
+        fields = [
+            'height',
+            'weight',
+            'blood_type',
+            'allergies',
+            'prescriptions',
+            'treatment_details',
+            'diagnoses',
+            'additional_notes',
+            'last_updated'
+            ]
+        
